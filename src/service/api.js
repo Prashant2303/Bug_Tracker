d06dd2350a5e43fe4acfd2253c2e00694b853373
Prashant2303 <prashant1997.official@gmail.com>
2021-05-29 22:02:49 +0530

    Changed api url

--- src/service/api.js
+++ src/service/api.js
@@ -1,2 +1,3 @@
-const base_url = 'http://localhost:3001';
+// const base_url = 'http://localhost:3001';
+const base_url = 'https://my-json-server.typicode.com/Prashant2303/Bug_Tracker_Data/';
 export default base_url;
