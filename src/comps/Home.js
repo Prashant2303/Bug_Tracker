7fadf5a7116b5e70cc2ccbd81281a39e5e6e5c83
Prashant Kumar <prashant.kumar.3@team.telstra.com>
2022-02-06 17:12:39 +0530

    Renamed and refactor v1

--- src/comps/Home.js
+++ src/comps/Home.js
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
-import { Button, Container, Paper, Grid } from '@material-ui/core';
+import { Container } from '@material-ui/core';
 import base_url from '../service/api';
 import IssuesList from './IssuesList';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { load } from '../redux/issueSlice';
-import { NavLink } from 'react-router-dom'
 import { loadUsers, login } from '../redux/userSlice';
 
-const AllIssues = () => {
+const Home = () => {
 
     const dispatch = useDispatch();
 
@@ -16,7 +15,7 @@ const AllIssues = () => {
         getIssuesFromServer();
         getUsersFromServer();
         getUserFromSession();
-    }, [])
+    })
 
     const getIssuesFromServer = () => {
         axios.get(`${base_url}/issues`).then(
@@ -52,7 +51,6 @@ const AllIssues = () => {
     }
     return (
         <>
-            {/* <h1>Issue List - Using Redux</h1> */}
             <Container>
                 <IssuesList />
             </Container>
@@ -60,4 +58,4 @@ const AllIssues = () => {
     )
 }
 
-export default AllIssues;
+export default Home;
