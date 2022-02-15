feec3521c73a6a0312363cf3885e96a87f25eb3b
Prashant Kumar <prashant.kumar.3@team.telstra.com>
2022-02-16 01:50:42 +0530

    Cleaned IssueSlice

--- src/redux/issueSlice.js
+++ src/redux/issueSlice.js
@@ -71,34 +71,6 @@ export const counterSlice = createSlice({
     isLoading: true
   },
   reducers: {
-    // load: (state, action) => {
-    //   state.list = action.payload;
-    // },
-    // add : (state, action) => {
-    //   state.list.push(action.payload);
-    //   console.log(JSON.stringify(state.list));
-    //   // increment: (state) => {
-    //   //   // Redux Toolkit allows us to write "mutating" logic in reducers. It
-    //   //   // doesn't actually mutate the state because it uses the Immer library,
-    //   //   // which detects changes to a "draft state" and produces a brand new
-    //   //   // immutable state based off those changes
-    //   //   state.value += 1
-    //   // },
-    // },
-    // del: (state, action) => {
-    //   let index;
-    //   // console.log(action.payload);
-    //   for (let i = 0; i < state.list.length; i++) {
-    //     // console.log(JSON.stringify(state.list[i]));
-    //     if (state.list[i].id === action.payload) {
-    //       index = i;
-    //       // console.log(JSON.stringify(i+" "+state.list[i]));
-    //       break;
-    //     }
-    //   }
-    //   let deletedIssue = state.list.splice(index, 1);
-    //   // console.log(JSON.stringify(index+" "+deletedIssue));
-    // }
   },
   extraReducers: {
     [getIssuesThunk.fulfilled]: (state, action) => {
@@ -123,7 +95,5 @@ export const counterSlice = createSlice({
 
   }
 })
-// Action creators are generated for each case reducer function
-export const { load, add, del } = counterSlice.actions
 
 export default counterSlice.reducer
